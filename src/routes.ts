a8e233ae1ff83e8e007b9bb35ad56512ff5be5c6
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-03 16:43:29 -0300

    Feat: add CRUD methods to routes and import routes to server

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,78 @@
 import { Router } from 'express';
+import {
+  createLetter, listOneLetter, listAllLetters, updateLetter, deleteLetter,
+} from './controller/LetterController';
+import { mustBeInteger, checkFields } from './service/LetterService';
 
 const router = Router();
+const baseURL = process.env.BASE_URL;
 
-router.get('/letters', (request, response) => {
-  response.json({ message: 'Hello World' });
-}); // Get one
-/**
- router.get(); // Get all
- router.post(); // Insert new letter
- router.put(); // Update letter
- router.delete(); // Delete letter
-*/
+router.get(`${baseURL}/:id`, mustBeInteger, async (request, response) => {
+  const { id } = request.params;
+
+  await listOneLetter(id)
+    .then((letter) => response.json(letter))
+    .catch((error) => {
+      if (error.status) {
+        response.status(error.status).json({ message: error.message });
+      } else {
+        response.status(500).json({ message: error.message });
+      }
+    });
+});
+
+router.get(`${baseURL}`, async (request, response) => {
+  await listAllLetters()
+    .then((letters) => response.json(letters))
+    .catch((error) => {
+      if (error.status) {
+        response.status(error.status).json({ message: error.message });
+      } else {
+        response.status(500).json({ message: error.message });
+      }
+    });
+});
+
+router.post(`${baseURL}`, checkFields, async (request, response) => {
+  await createLetter(request.body)
+    .then((letter) => response.status(201).json({
+      message: 'Your letter was created successfully!',
+      content: letter,
+    }))
+    .catch((error) => response.status(500).json({ message: error.message }));
+});
+
+router.put(`${baseURL}/:id`, mustBeInteger, checkFields, async (request, response) => {
+  const { id } = request.params;
+
+  await updateLetter(id, request.body)
+    .then((letter) => response.json({
+      message: 'Your letter was updated successfully!',
+      content: letter,
+    }))
+    .catch((error) => {
+      if (error.status) {
+        response.status(error.status).json({ message: error.message });
+      } else {
+        response.status(500).json({ message: error.message });
+      }
+    });
+});
+
+router.delete(`${baseURL}/:id`, mustBeInteger, async (request, response) => {
+  const { id } = request.params;
+
+  await deleteLetter(id)
+    .then(() => response.json({
+      message: 'Your letter was deleted successfully!',
+    }))
+    .catch((error) => {
+      if (error.status) {
+        response.status(error.status).json({ message: error.message });
+      } else {
+        response.status(500).json({ message: error.message });
+      }
+    });
+});
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import router from './routes';
 const server = express();
 
 server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 server.use(router);
 
 server.listen(3030, () => console.log('Server is running'));
