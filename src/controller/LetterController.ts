570bc0c78f2a62bb93c0834fbe8435624331c5a9
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-03 14:42:52 -0300

    Feat: create LetterController

---
+++ src/controller/LetterController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+
+class LetterController {
+  create(request: Request, response: Response) {
+
+  }
+
+  listOne(request: Request, response: Response) {
+
+  }
+
+  listAll(request: Request, response: Response) {
+
+  }
+
+  update(request: Request, response: Response) {
+
+  }
+
+  delete(request: Request, response: Response) {
+
+  }
+}
+
+export default LetterController;
